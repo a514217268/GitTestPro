223d224224e9412a8b7ea3fa6ea22c9620dd4374
夏梦雷 <490442541@qq.com>
2016-04-28 11:15:18 +0800

    只是一个新项目

---
+++ GitCodePro/GitCodePro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitCodePro
+//
+//  Created by 夏梦雷 on 16/4/28.
+//  Copyright © 2016年 夏梦雷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitCodePro/GitCodePro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitCodePro
+//
+//  Created by 夏梦雷 on 16/4/28.
+//  Copyright © 2016年 夏梦雷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
